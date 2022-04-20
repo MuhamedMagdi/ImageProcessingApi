a757ab9fe374cd39a493a29a5efb2c17cff6617a
MuhamedMagdi <m49dii@gmail.com>
2022-04-20 06:12:35 +0200

    Middelwares for checking the url

---
+++ src/middleware/image.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import fs from 'fs-extra';
+import path from 'path';
+
+const imageExist = async (req: Request, res: Response, next: NextFunction) => {
+    const { imagename } = req.params;
+    const imagepath = path.join(
+        __dirname,
+        '../../',
+        'media',
+        'images',
+        imagename
+    );
+    const exists = await fs.pathExists(imagepath);
+    if (exists) {
+        next();
+    } else {
+        res.status(404).send('Image not found');
+    }
+};
+
+const imageExtension = (req: Request, res: Response, next: NextFunction) => {
+    const { imagename } = req.params;
+    const extension = imagename.split('.')[1];
+    if (extension === undefined) {
+        res.status(400).send('You should provide the extension of an image');
+    } else if (extension !== 'jpg') {
+        res.status(400).send('jpg is the only supported extension');
+    } else {
+        next();
+    }
+};
+
+export { imageExist, imageExtension };
