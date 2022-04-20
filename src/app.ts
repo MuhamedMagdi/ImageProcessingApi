808351d196eb59d5107c045a09fa3bd120c4ee80
MuhamedMagdi <m49dii@gmail.com>
2022-04-20 06:00:32 +0200

    Configure the server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import router from './routes/index';
 
 const app = express();
 const port = 3000;
 
+app.use('/api', router);
+
 app.listen(port, () => {
     console.log(`The server is listening on port ${port}...`);
 });
+
+export default app;
